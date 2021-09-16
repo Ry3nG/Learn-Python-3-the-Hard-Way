5523672a829849ecd3d5fa15b424c75c64194d46
Ry3n <Nomlasssssss@outlook.com>
2021-09-17 01:11:10 +0800

    <commit> initial commit on ex5

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Ryan.G'
+my_age = 18  #not a lie
+my_height = 178  # cm
+my_weight = 65  # kg
+my_eyes = 'Dark Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kilos heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right.
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_weight}, and {my_height} I get {total}.")
