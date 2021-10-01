e9dbd87b5051a34c661b7c18eeb581463abdc720
Ry3n <Nomlasssssss@outlook.com>
2021-10-01 22:35:19 +0800

    <fix>fixed format issues

--- ex35.py
+++ ex35.py
@@ -1,20 +1,22 @@
 from sys import exit
 
+
 def gold_room():
     print("This room is full of gold. How much do you take?")
 
     choice = input(">>>")
     if "0" in choice or "1" in choice:
-        how_much  = int(choice)
+        how_much = int(choice)
     else:
         dead("Man, learn to type a number.")
-    
+
     if how_much < 50:
         print("Nice, you're not greedy, you win!")
         exit(0)
     else:
         dead("You greedy bastard!")
-    
+
+
 def bear_room():
     print("There is a bear here.")
     print("The bear has a bunch of honey.")
@@ -38,6 +40,7 @@ def bear_room():
         else:
             print("I got no idea what that means.")
 
+
 def cthulhu_room():
     print("Here you see the great evil Cthulhu.")
     print("He, it, whatever stares at you and you go insane.")
@@ -52,9 +55,11 @@ def cthulhu_room():
     else:
         cthulhu_room()
 
+
 def dead(why):
     print(why, "Good job!")
     exit(0)
+
 
 def start():
     print("You are in a dark room.")
@@ -69,5 +74,6 @@ def start():
         cthulhu_room()
     else:
         dead("You stumble around the room until you starve.")
-    
+
+
 start()
